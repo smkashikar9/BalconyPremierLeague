08b5aaae2ac51173e797c978912ec1e1fbda4840
smkashikar9 <62503681+smkashikar9@users.noreply.github.com>
2023-06-18 20:41:43 +0000

    new login mechanism
    added login and registration pages. changed the run config to app.py and added app.py and forms.py

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-from flask import Flask, render_template, redirect, url_for, request
+# from flask import Flask, render_template, redirect, url_for, request
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
+# # Route for handling the login page logic
+# @app.route('/', methods=['GET', 'POST'])
+# def login():
+#   error = None
+#   if request.method == 'POST':
+#     if request.form['username'] != 'admin' or request.form[
+#         'password'] != 'admin':
+#       error = 'Invalid Credentials. Please try again.'
+#     else:
+#       return redirect(url_for('home'))
+#   return render_template('login.html', error=error)
 
-# Route for handling the login page logic
-@app.route('/', methods=['GET', 'POST'])
-def login():
-  error = None
-  if request.method == 'POST':
-    if request.form['username'] != 'admin' or request.form[
-        'password'] != 'admin':
-      error = 'Invalid Credentials. Please try again.'
-    else:
-      return redirect(url_for('home'))
-  return render_template('login.html', error=error)
+# @app.route("/home")
+# def home():
+#   return render_template('home.html')
 
-
-@app.route("/home")
-def home():
-  return render_template('home.html')
-
-
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+# if __name__ == '__main__':
+#   app.run(host='0.0.0.0', debug=True)
